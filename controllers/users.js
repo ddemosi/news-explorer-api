891a39fa6f188c04d5c5fb84a41a810f18e682d4
Daniel Gummow <all54735@adobe.com>
2021-01-25 16:51:20 -0700

    updated cookies to https

--- controllers/users.js
+++ controllers/users.js
@@ -54,7 +54,7 @@ function createUser(req, res, next) {
 
           const newObj = { name: result.name, email: result.email, token };
 
-          res.cookie('token', `Bearer ${token}`, { expires: new Date(Date.now() + 24 * 3600000), sameSite: 'none' });
+          res.cookie('token', `Bearer ${token}`, { expires: new Date(Date.now() + 24 * 3600000), sameSite: 'none', secure: true });
           res.status(200).send(newObj);
         })
         .catch(next);
@@ -79,7 +79,7 @@ function login(req, res, next) {
         { _id: user._id },
         process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' },
       );
-      res.cookie('token', `Bearer ${token}`, { expires: new Date(Date.now() + 24 * 3600000), sameSite: 'none' });
+      res.cookie('token', `Bearer ${token}`, { expires: new Date(Date.now() + 24 * 3600000), sameSite: 'none', secure: true });
       res.status(200).send({ token: `Bearer ${token}` });
     })
     .catch(next);
