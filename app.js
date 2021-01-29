24d849249606e9d6e005aa7f2d7038fcbc30183b
Daniel Gummow <all54735@adobe.com>
2021-01-29 15:43:20 -0700

    added cookieparser

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { errors } = require('celebrate');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -43,7 +43,7 @@ app.use(requestLogger);
 
 app.use(express.json());
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // assign the corresponding header to the origin variable
